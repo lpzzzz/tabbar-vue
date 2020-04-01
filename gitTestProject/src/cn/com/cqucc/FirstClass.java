7dd96d1682eb7c0202a64fc506abce232e8a8e85
peng.yan.lu <14787005750@163.com>
2020-04-01 23:02:08 +0800

    修改之后

---
+++ gitTestProject/src/cn/com/cqucc/FirstClass.java
@@ -0,0 +1,17 @@
+package cn.com.cqucc;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("测试Git!");
+    }
+
+    public void test() {
+        int i = 0;
+        System.out.println(i + 10);
+    }
+
+    public void testGit() {
+        int i = 10;
+        System.out.println(i + 10);
+    }
+}
